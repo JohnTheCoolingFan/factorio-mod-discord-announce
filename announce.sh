606c0dc6aa7405476c28a5bac9092bd7abd3445c
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-07-28 18:54:33 +0300

    Escape discord list syntax

--- announce.sh
+++ announce.sh
@@ -2,6 +2,6 @@
 
 MOD_NAME=$(jq -r ".name" "$INFO_JSON_FILE")
 MOD_VERSION=$(jq -r ".version" "$INFO_JSON_FILE")
-CHANGELOG_TEXT="${MOD_NAME} ${MOD_VERSION} has been released. Changelog:"$'\n'"$(sed -n '/\-\{99,\}/{n; p; :loop n; /\-\{99,\}/q; p; b loop}' ${CHANGELOG_FILE} | sed 's/\(.*\)/> &/')"
+CHANGELOG_TEXT="${MOD_NAME} ${MOD_VERSION} has been released. Changelog:"$'\n'"$(sed -n '/\-\{99,\}/{n; p; :loop n; /\-\{99,\}/q; p; b loop}' ${CHANGELOG_FILE} | sed 's/\(.*\)/> &/' | sed 's/- /\\- /')"
 
 curl -H 'Content-Type: multipart/form-data' --form-escape -F username="${MOD_NAME}" -F content="${CHANGELOG_TEXT}" $DISCORD_WEBHOOK
